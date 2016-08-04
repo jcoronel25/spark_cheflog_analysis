0d78cab4ddedcc25d8b138b80ad9eff6052965fd
Juan Carlos <jcoronel25@users.noreply.github.com>
2016-08-04 16:31:17 -0400

    test second commit

--- notebooks/Users/jcoronel@its.jnj.com/chef_logs.py
+++ notebooks/Users/jcoronel@its.jnj.com/chef_logs.py
@@ -1,1 +1,9 @@
-# Databricks notebook source exported at Thu, 4 Aug 2016 20:27:50 UTC
+# Databricks notebook source exported at Thu, 4 Aug 2016 20:31:03 UTC
+# MAGIC %fs ls /FileStore/tables/pwymmu451470340071320/chef_client-ba119.log
+
+# COMMAND ----------
+
+df1 = sqlContext.read.text("/FileStore/tables/pwymmu451470340071320/chef_client-ba119.log")
+
+# COMMAND ----------
+
